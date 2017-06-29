8ad83ddc7f5c8342f2d3deccee7500a734911494
Alex Rodriguez Cid <alexrcid@hotmail.com>
2017-06-29 14:18:30 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,216 +1,156 @@
-function Student() {
-  this.direction = "left";
-  this.body = [
-    { row: 47, column: 24 }
-  ];
-}
-
-
-
-function Item (){
-  this.position =
-    {row: 0, column: Math.floor(Math.random() * 50)};
-}
-
-
-Student.prototype.move = function () {
-  var head = this.body[0];
-
-  switch (this.direction) {
-    case "left":
-    this.body.unshift({
-      row: head.row,
-      column: (head.column - 1 )
-    });
-      break;
-
-      case "right":
-      this.body.unshift({
-        row: head.row,
-        column: (head.column + 1)
-      });
-    break;
-  }
-  this.body.pop();
-};
-
-Item.prototype.move = function () {
-  this.position.row =  this.position.row + 1;
-};
-
-
-Game.prototype.checkPosition = function(){
-  var itemCurrentPosition = this.item.position;
-  var studentCurrentPosition = this.student.body[0];
-
-  if(itemCurrentPosition.row === studentCurrentPosition.row &&
-     itemCurrentPosition.column === studentCurrentPosition.column){
-    console.log("Picked");
-  } else {
-    console.log("Not yet");
-  }
-
-};
-
-Student.prototype.goLeft = function() {
-   if (this.direction === 'left' || this.direction === 'right') {
-     this.direction = 'left';
-   }
- };
-
- Student.prototype.goRight = function() {
-   if (this.direction === 'right' || this.direction === 'left') {
-     this.direction = 'right';
-   }
- };
-
-// Game.prototype.buclee = function(){
-//   var i = 0;
-// var intervalId = setInterval(function () {
-//   console.log(i);
-//   console.log("do something");
-//   i++;
-//
-//   if (i > 10) {
-//     clearInterval(intervalId);
-//   }
-// }.bind(this), 1000);
-// };
-
-
 function Game() {
-  this.student = new Student();
-  this.items = []
-  // this.colection = [];
+  this.items = [];
 
    for (var row = 0; row < 50; row++) {
      for (var col = 0; col < 50; col++) {
-       $('.container').append($('<div>')
+       $('#board').append($('<div>')
           .addClass('cell')
           .attr('data-row', row)
           .attr('data-col', col)
         );
      }
    }
-   this.drawItem();
-   this.drawStudent();
-   this.assignControlsToKeys();
-  //  this.buclee();
-  // this.generatorItem();
  }
 
-//  Game.prototype.generatorItem = function () {
-//
-//    var i = this.drawItem();
-//    var intervalId = setInterval(function () {
-//      console.log(i);
-//
-//      i++;
-//
-//      if (i > 10) {
-//        clearInterval(intervalId);
-//      }
-//    }, 1000);
-// };
-Game.prototype.drawStudent = function() {
-     this.student.body.forEach(function(position, index) {
-       var selector = '[data-row=' + position.row + ']' +
-                      '[data-col=' + position.column + ']';
-
-      $(selector).addClass('student');
-    });
-  };
-
-Game.prototype.drawItem = function() {
-
-
-
-
-    this.items.forEach(function(item, index) {
-    var selector = '[data-row=' + item.position.row + ']' +
-                   '[data-col=' + item.position.column + ']';
-      $(selector).addClass('item');
-    });
-  };
-
-Game.prototype.clearStudent = function() {
-   $('.student').removeClass('student'); };
-
-Game.prototype.clearItem = function() {
-      $('.item').removeClass('item'); };
-
- Game.prototype.start = function() {
-    if (!this.intervalId) {
-      this.intervalId = setInterval(this.update.bind(this), 1000);
+Game.prototype.createBoard = function(){
+  for (var row = 0; row < 50; row++) {
+    for (var col = 0; col < 50; col++) {
+      $('#board').append($('<div>')
+                 .addClass('cell')
+                 .attr('data-row', row)
+                 .attr('data-col', col)
+      );
     }
-  };
+  }
+};
 
- Game.prototype.update = function() {
-   var item = new Item();
-   if(this.items.length === 0) this.items.push(item);
+Game.prototype.start = function(){
+  this.student = new Student();
+  this.student.show();
+  this.student.move();
+  this.assignControlsToKeys();
+  this.createItem();
+  this.update();
+};
 
-   var counter = 0;
+Game.prototype.checkIfStudentCatchTheItem = function(itemCloseToTheStudent){
+  if (this.student.position.row === itemCloseToTheStudent.position.row && this.student.position.col === itemCloseToTheStudent.position.col) {
+    console.log(" ================  YEAAAH   =================");
+  } else {
+    console.log('No collision...   :( ');
+  }
+};
 
-   this.start = function(){
-     var item = this.items[counter];
-     if(item.position.row > 45){
-       console.log("El item ha pasado la fila 45");
-       this.checkPosition();
-     }
-     item.move();
-     this.student.move();
-     this.clearStudent();
-     this.clearItem();
-     this.drawStudent();
-     this.drawItem();
+// Game.prototype.studentCollision = function(){
+//
+//   var a, b, c;
+//   a = this.student.position[1];
+//   b = col[0];
+//   c = col[50];
+//
+//   function collisionDetection() {
+//   if( a === b){
+//     return alert("Its collisioning at left");
+//   }else if( a === c){
+//     return alert("Its collisioning at right");
+//       }
+//     }
+//   };
 
-     if (counter < this.items.length) {
-       this.start();
-     }
+Game.prototype.update = function(){
+  this.itemInterval = setInterval(this.createItem.bind(this),2000); // Item generation time
+  this.interval = setInterval(this.updateItem.bind(this), 400); // Item Speed
+};
 
-   }.bind(this);
-
-   this.start();
- };
-
- Game.prototype.stop = function() {
-    if (this.intervalId) {
-     clearInterval(this.intervalId);
-      this.intervalId = undefined;
-   }
-  };
-
- Game.prototype.assignControlsToKeys = function() {
-   $('body').on('keydown', function(e) {
-     switch (e.keyCode) {
-       case 37: // arrow left
-         this.student.goLeft();
-        break;
-       case 39: // arrow right
-          this.student.goRight();
-          break;
-      case 65: // A goes left
+Game.prototype.assignControlsToKeys = function() {
+  $(document).on('keydown', function(e) {
+    switch (e.keyCode) {
+      case 37: // arrow left
         this.student.goLeft();
-      break;
-      case 68: // D goes right
+        break;
+      case 39: // arrow right
         this.student.goRight();
         break;
-       case 32: // p pause
-            if (this.intervalId) {
-            this.stop();
-           } else {
-            this.start();
-          }
-            break;
-     }
-   }.bind(this));
- };
+    }
+  }.bind(this));
+};
 
+Game.prototype.createItem = function(){
+  this.items.push(new Item());
+};
 
+Game.prototype.updateItem = function(){
+  var currentItem;
+  for(var i=0; i < this.items.length; i++) {
+    currentItem = this.items[i];
+    currentItem.move();
+    if( currentItem.position.row > 46 && currentItem.position.row < 48){
+      this.checkIfStudentCatchTheItem(currentItem);
+    } // End if
+  } // End for
+};
+
+// Student Constructor
+function Student() {
+  this.direction = "left";
+  this.position  = { row: 47, col: 24 };
+}
+
+// Student move
+Student.prototype.move = function () {
+  switch (this.direction) {
+    case "left":
+      this.position.col -= 1;
+      this.show();
+      setTimeout(this.move.bind(this), 80);
+      break;
+    case "right":
+      this.position.col += 1;
+      this.show();
+      setTimeout(this.move.bind(this), 80);
+      break;
+  }
+};
+
+Student.prototype.goLeft = function() {
+  if (this.direction === 'left' || this.direction === 'right') {
+    this.direction = 'left';
+  }
+};
+
+Student.prototype.goRight = function() {
+  if (this.direction === 'right' || this.direction === 'left') {
+    this.direction = 'right';
+  }
+};
+
+// Show Student in the browser
+Student.prototype.show = function() {
+  $('.student').removeClass('student');
+  var selector = '[data-row=' + this.position.row + ']' +
+                 '[data-col=' + this.position.col + ']';
+
+  $(selector).addClass('student');
+};
+
+// Item Constructor
+function Item (){
+  this.position = {row: 0, col: Math.floor(Math.random() * 50)};
+}
+
+// Item move
+Item.prototype.move = function () {
+  this.selector = function(row = this.position.row) {
+    return '[data-row=' + row + ']' + '[data-col=' + this.position.col + ']';
+  };
+  this.position.row = this.position.row + 1;
+  $(this.selector(this.position.row - 1)).removeClass('item');
+  $(this.selector()).addClass('item');
+};
 
 
 var game;
- $(document).ready(function() {
-   game = new Game();
-   game.start();
-  });
+$(document).ready(function(){
+  game = new Game();
+  game.start();
+});
